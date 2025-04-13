471f04842b862bab5285027666f7d8850370536c
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2025-04-13 15:31:51 -0300

    hello world

---
+++ src/main/kotlin/si/rsamodel/RsAmodelApplication.kt
@@ -0,0 +1,11 @@
+package si.rsamodel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsAmodelApplication
+
+fun main(args: Array<String>) {
+    runApplication<RsAmodelApplication>(*args)
+}
---
+++ src/test/kotlin/si/rsamodel/RsAmodelApplicationTests.kt
@@ -0,0 +1,13 @@
+package si.rsamodel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RsAmodelApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
